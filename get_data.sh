4917cb6153241222bde359e0db80d3f87195e1ff
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2020-12-23 15:43:00 +0100

    Minor changes

--- get_data.sh
+++ get_data.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-mkdir data
+data_dir=data
+mkdir $data_dir
 
 echo "Scikit-learn..."
 wget -O sklearn.zip https://github.com/scikit-learn/scikit-learn/archive/master.zip
-unzip -q -d sklearn sklearn.zip
-mv sklearn source_data
+unzip -q -d $data_dir/sklearn sklearn.zip
 
 echo "Tensorflow..."
 wget -O tensorflow.zip https://github.com/tensorflow/tensorflow/archive/master.zip
-unzip -q -d tensorflow tensorflow.zip
-mv tensorflow source_data
+unzip -q -d $data_dir/tensorflow tensorflow.zip
 
 echo "PyTorch..."
 wget -O pytorch.zip https://github.com/pytorch/pytorch/archive/master.zip
-unzip -q -d pytorch pytorch.zip
-mv pytorch source_data
+unzip -q -d $data_dir/pytorch pytorch.zip
 
 echo "Cleanup..."
 rm sklearn.zip
